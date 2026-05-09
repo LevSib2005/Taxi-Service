166f24b7dc875a0631b65c9b8b6db3e720fabecf
LevSib2005 <kengymsen@gmail.com>
2026-05-09 15:56:35 +0700

    Добавил сущности trip

---
+++ trip-service/src/main/java/com/example/trip_service/entity/Trip.java
@@ -0,0 +1,43 @@
+package com.example.trip_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "trips")
+@Data
+public class Trip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long passengerId;
+    private Long driverId;
+
+    @Enumerated(EnumType.STRING)
+    private TripStatus status = TripStatus.CREATED;
+
+    private String origin;
+    private String destination;
+    private Double price;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public enum TripStatus {
+        CREATED,
+        ACCEPTED,
+        STARTED,
+        COMPLETED,
+        CANCELLED
+    }
+}
